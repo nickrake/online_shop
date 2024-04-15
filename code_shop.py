d861a9d61e169e43c30791fd7d93ce71dfd3f27e
Nick Pervoy <pervoy.nikita@gmail.com>
2024-04-15 20:13:27 +0300

    розділення коду пайтон та sql

--- code_shop.py
+++ code_shop.py
@@ -1,150 +1,123 @@
+from sql_scripts import ShopDB
 import sqlite3
 
+db = ShopDB("shop.db")
+
+connection = sqlite3.connect("shop.db")
+cursor = connection.cursor()
+
+
 def search_game():
-     print("Введіть назву відео гри")
-     game_title = input(":").lower()
-     cursor.execute('''SELECT quantity, title, price, description, category, developer, platform FROM items WHERE title = ?''', (game_title,))
-     data = cursor.fetchall()
-     if data:
-           for row in data:
-                     quantity, title, price, description, category, developer, platform = row
-                     print("Назва гри:", title, "\n")
-                     print("Ціна:", price, "\n")
-                     print("Розробник:", developer, "\n")
-                     print("Жанр:", category, "\n")
-                     print("Опис:", description, "\n")
-                     print("Платформа:", platform, "\n")
-                     print("Кількість на складі:", quantity, "\n")
-                               
-     else:
-           print("Відео гра з такою назвою не знайдена", "\n")
+    print("Введіть назву відео гри")
+    game_title = input(":").lower()
+    if db.search_email(game_title):
+        for row in db.search_email(game_title):
+            quantity, title, price, description, category, developer, platform = row
+            print("Назва гри:", title, "\n")
+            print("Ціна:", price, "\n")
+            print("Розробник:", developer, "\n")
+            print("Жанр:", category, "\n")
+            print("Опис:", description, "\n")
+            print("Платформа:", platform, "\n")
+            print("Кількість на складі:", quantity, "\n")
+
+    else:
+        print("Відео гра з такою назвою не знайдена", "\n")
 
 
 def create_account():
-       print("Введіть своє імя")
-       name = input(":")
+    print("Введіть своє імя")
+    name = input(":")
 
-       print("Введіть свою фамілію")
-       surname = input(":")
+    print("Введіть свою фамілію")
+    surname = input(":")
 
-       print("Введіть свій пароль")
-       password = input(":")
+    print("Введіть свій пароль")
+    password = input(":")
 
-       while True:
-              print("Введіть свій емейл")
-              email = input(":")
-              cursor.execute('''SELECT email FROM clients WHERE email=?''', [email])
-              is_email = cursor.fetchone()
+    while True:
+        print("Введіть свій емейл")
+        email = input(":")
+        if db.is_email_exist(email):
+            print("Аккаунт з таким емейлом вже існує", "\n")
+        else:
+            break
 
-              if is_email:
-                     print("Аккаунт з таким емейлом вже існує", "\n")
-              else:
-                     break
+    while True:
+        print("Введіть свій номер телефону")
+        phone = input(":")
 
-       while True:
-              print("Введіть свій номер телефону")
-              phone = input(":")
-              cursor.execute('''SELECT phone FROM clients''')
-              data = [row[0] for row in cursor.fetchall()]                #?????????????????????
+        if db.is_phone_exist(phone):
+            print("Аккаунт з таким номером телефону вже існує", "\n")
+        else:
+            break
 
-              if phone in data:
-                     print("Аккаунт з таким номером телефону вже існує", "\n")
-              else:
-                     break
-              
-       while True:
-              print("Чи ви хочете ввести місто? (не обовязково)", "\n")
-              print("y - так")
-              print("n - ні")
-              q = input(":")
-              if q == "y":
-                     print("Введіть своє місто")
-                     city = input(":")
-                     break
+    while True:
+        print("Чи ви хочете ввести місто? (не обовязково)", "\n")
+        print("y - так")
+        print("n - ні")
+        q = input(":")
+        if q == "y":
+            print("Введіть своє місто")
+            city = input(":")
+            break
 
-              elif q == "n":
-                     city = None
-                     break
-              
-              else:
-                     print("Такоі команди не існує", "\n")
-                     
+        elif q == "n":
+            city = None
+            break
 
+        else:
+            print("Такоі команди не існує", "\n")
 
-       cursor.execute('''INSERT INTO clients("name", "surname", "email", "phone", "city", "password")
-                      VALUES(?,?,?,?,?,?)''',
-                      [name,surname,email,phone,city,password])
-       
-       connection.commit()
+    db.create_user(name, surname, email, phone, city, password)
+    print("Вітаємо ви створили аккаунт!", "\n")
 
-       print("Вітаємо ви створили аккаунт!", "\n")
-                      
 
 def show_game_list():
-      cursor.execute("""SELECT title, price FROM items""")
-      data = cursor.fetchall()
-      for row in data:
-            title, price = row
-            print("Назва гри:", title, "- Ціна:", price)
-      
+    for row in db.all_games_and_price():
+        title, price = row
+        print("Назва гри:", title, "- Ціна:", price)
+
 
 def loggin():
     while True:
         print("Введіть свій емейл")
         email = input(":")
-        cursor.execute('''SELECT password FROM clients WHERE email = ?''', (email,))
-        data = cursor.fetchone()  
+        print("Введіть свій пароль")
+        password = input(":")
 
-        if data:
-            print("Введіть свій пароль")
-            password = input(":")
-            if password == data[0]: 
-                print("Вітаємо, ви увійшли в свій аккаунт!", "\n")
-                break
-            else:
-                print("Пароль для цього аккаунту введений неправильно", "\n")
+        if db.check_email_and_password(email, password):
+            print("Вітаємо, ви увійшли в свій аккаунт!", "\n")
+            break
         else:
-            print("Аккаунт з таким емейлом не існує")
+            print("Пароль або емпейл для цього аккаунту введений неправильно", "\n")
 
 
 def add_to_cart():
-      print("Який товар ви хочете додати до кошика")  
-      item =input(":")
-      cursor.execute('''SELECT id FROM items WHERE title = ?''', [item])
-      item_db = cursor.fetchone()
-      if item_db:
-            
-
-
-
+    print("Який товар ви хочете додати до кошика")
+    item = input(":")
+    if db.items_id(item):
+        cursor
 
 
 def is_logged():
-       while True:
-              print("Яку дію ви хочете виконати?\n")
-              print("1 - пошук відео гри по назві")
-              print("2 - відобразити список ігор")
-              print("3 - вихід з аккаунту")
-              q = input(":")
-              if q == "1":
-                     search_game()
-              
-              elif q == "2":
-                    show_game_list()
-                    
-              elif q == "3":
-                     break
-              else:
-                     print("Такої команди не існує")
-        
+    while True:
+        print("Яку дію ви хочете виконати?\n")
+        print("1 - пошук відео гри по назві")
+        print("2 - відобразити список ігор")
+        print("3 - вихід з аккаунту")
+        q = input(":")
+        if q == "1":
+            search_game()
 
+        elif q == "2":
+            show_game_list()
 
-         
+        elif q == "3":
+            break
+        else:
+            print("Такої команди не існує")
 
-
-
-connection = sqlite3.connect("shop.db")
-cursor = connection.cursor()
 
 
 
@@ -157,14 +130,14 @@ while True:
     q = input(":")
 
     if q == "1":
-          create_account()
-          is_logged()
-    
+        create_account()
+        is_logged()
+
     elif q == "2":
-           loggin()
-           is_logged()
+        loggin()
+        is_logged()
     elif q == "3":
-             break
-    
+        break
+
     else:
         print("Такої команди не існує", "\n")
---
+++ sql_scripts.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+class ShopDB:
+    def __init__(self, dbname):
+        self.connection = sqlite3.connect(dbname)
+        self.cursor = self.connection.cursor()
+
+    def is_email_exist(self, email):
+        self.cursor.execute('''SELECT email FROM clients WHERE email=?''', [email])
+        is_email = self.cursor.fetchone()
+        return is_email
+    
+    def is_phone_exist(self, phone):
+        self.cursor.execute('''SELECT phone FROM clients WHERE phone=?''', [phone])
+        is_phone = self.cursor.fetchone()
+        return is_phone
+    
+    def create_user(self,name,surname,email,phone,city,password):
+        self.cursor.execute('''INSERT INTO clients("name", "surname", "email", "phone", "city", "password")
+                      VALUES(?,?,?,?,?,?)''',
+                      [name,surname,email,phone,city,password])
+       
+        self.connection.commit()
+
+    def check_email_and_password(self, email, password):
+        self.cursor.execute('''SELECT password FROM clients WHERE email = ? AND password = ?''', (email, password))
+        user = self.cursor.fetchone()
+        return user
+
+    def search_email(self, game_title):
+        self.cursor.execute(
+        '''SELECT quantity, title, price, description, category, developer, platform FROM items WHERE title = ?''', (game_title,))
+        data = self.cursor.fetchall()
+        return data
+    def all_games_and_price(self):
+        self.cursor.execute("""SELECT title, price FROM items""")
+        data = self.cursor.fetchall()
+        return data
+    
+    def items_id(self, item):
+        self.cursor.execute('''SELECT id FROM items WHERE title = ?''', [item])
+        item_db = self.cursor.fetchone()
+        return item_db
