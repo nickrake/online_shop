fcc20d515a563dabe7ba07a0902d7ea216818bda
Nick Pervoy <pervoy.nikita@gmail.com>
2024-04-17 18:59:46 +0300

    . Переробив код щоб я його розумів                                           . зробив підписи пояснення для себе                                                . додав нову функцію додавання ігри до кошику

--- code_shop.py
+++ code_shop.py
@@ -1,12 +1,44 @@
+#підключення классу ShopDB з файлу sql_scripts для взаємодії з базою даних
 from sql_scripts import ShopDB
-import sqlite3
 
+#створення обьекту классу ShopDB
 db = ShopDB("shop.db")
 
-connection = sqlite3.connect("shop.db")
-cursor = connection.cursor()
+#стан заказу
+current_order = None
+#стан користувача
+current_user = None
+
+#створення классу "замовлення"
+class Order:
+
+    #параметри обьекту
+    def __init__(self, client_id):
+        self.client_id = client_id
+        self.cart_items = []
+        self.id = db.create_order(client_id)
+
+    #функція додавання в кошик предмету
+    def add_to_cart(self, cart_item, quantity):
+        item_db = db.items_id(cart_item)
+        if item_db:
+            db.add_item_to_cart(self.id, item_db[0], quantity)
+            price = float(db.game_price(cart_item)[0])
+            total_price = price * float(quantity)
+            self.cart_items.append({"Назва":cart_item, "Кількість":quantity, "Вартість":total_price})
+            print("Товар додано в кошик")
+            print(self.cart_items)
+        else:
+            print("Помилка")
+
+    #функція показу вмісту кошика
+    def show_cart(self):
+        for item in self.cart_items:
+            print(f"Товар: {item['Назва']}, Кількість:{item['Кількість']} - {item['Вартість']} грн")
 
 
+
+#функція програми по пошуку гри по назві
 def search_game():
     print("Введіть назву відео гри")
     game_title = input(":").lower()
@@ -25,6 +57,8 @@ def search_game():
         print("Відео гра з такою назвою не знайдена", "\n")
 
 
+
+#функція програми по створення аккаунту
 def create_account():
     print("Введіть своє імя")
     name = input(":")
@@ -68,17 +102,24 @@ def create_account():
 
         else:
             print("Такоі команди не існує", "\n")
-
+    
+    
     db.create_user(name, surname, email, phone, city, password)
     print("Вітаємо ви створили аккаунт!", "\n")
+    id = db.search_id_in_emails(email)
+    return id
 
 
+
+#функція програми по друкуванню списка всіх ігор та ціни
 def show_game_list():
     for row in db.all_games_and_price():
         title, price = row
         print("Назва гри:", title, "- Ціна:", price)
 
 
+
+#функція програми по реестрація в існуючій акаунт
 def loggin():
     while True:
         print("Введіть свій емейл")
@@ -86,66 +127,91 @@ def loggin():
         print("Введіть свій пароль")
         password = input(":")
 
-        if db.check_email_and_password(email, password):
+        user = db.check_email_and_password(email, password)
+        if user:
             print("Вітаємо, ви увійшли в свій аккаунт!", "\n")
-            break
+            id = db.search_id_in_emails(email)
+            return id
+     
         else:
             print("Пароль або емпейл для цього аккаунту введений неправильно", "\n")
 
 
-def add_to_cart():
-    while True:
-        print("Який товар ви хочете додати до кошика")
-        item = input(":")
-        item_id = db.items_id(item)
-        if item_id:
-            db.insert_id(item_id)
+
+#??? Навіщо потрібна фунція add_to_cart() якщо такаж функція є в класі Order ???
+
+# def add_to_cart():
+#     while True:
+#         print("Який товар ви хочете додати до кошика")
+#         item = input(":")
+#         print("Кількість")
+#         quantity = input(":")
+#         db.add_item_to_cart(item, quantity)
+
+#??? Навіщо потрібна фунція add_to_cart() якщо такаж функція є в класі Order ???
+
+
+
+#інтерфейс взаємодаї з користувачем
+while True:
+    if not current_user:
+        print("Яку дію ви хочете виконати?\n")
+        print("1 - створення аккаунту")
+        print("2 - вхід в аккаунту")
+        print("3 - вихід з програми")
+
+        q = input(":")
+
+        if q == "1":
+            id = create_account()
+            current_user = "logged"
+
+        elif q == "2":
+            id = loggin()
+            current_user = "logged"
+
+        elif q == "3":
+            break
+
         else:
-            print("Такого товару не існує")
-    
-
-    
-
-
-def is_logged():
-    while True:
+            print("Такої команди не існує", "\n")
+    elif current_user:
         print("Яку дію ви хочете виконати?\n")
         print("1 - пошук відео гри по назві")
         print("2 - відобразити список ігор")
-        print("3 - вихід з аккаунту")
+        print("3 - додати в кошик")
+        print("4 - вихід з аккаунту")
         q = input(":")
         if q == "1":
             search_game()
 
         elif q == "2":
             show_game_list()
-
+        
         elif q == "3":
-            break
+            if  not current_order:
+                current_order = Order(id[0])
+                while True:
+                    print("Що зробити?")
+                    print("1 - додати товар")
+                    print("2 - підтвердити покупки")
+                    q = input(":")
+                    if q == "1":
+                        print("Який товар ви хочете добавити")
+                        cart_item = input(":")
+                        print("Скільки цього товару добавити в кошик")
+                        quantity = input(":")
+                        current_order.add_to_cart(cart_item, quantity)
+                    elif q == "2":
+                        current_order = None
+                        break
+                    else:
+                        print("Такої команди не існує")
+            else:
+                print("Помилка")
+        elif q == "4":
+            current_user = None
         else:
             print("Такої команди не існує")
-
-
-
-add_to_cart()
-
-while True:
-    print("Яку дію ви хочете виконати?\n")
-    print("1 - створення аккаунту")
-    print("2 - вхід в аккаунту")
-    print("3 - вихід з програми")
-
-    q = input(":")
-
-    if q == "1":
-        create_account()
-        is_logged()
-
-    elif q == "2":
-        loggin()
-        is_logged()
-    elif q == "3":
-        break
-
     else:
-        print("Такої команди не існує", "\n")
+        print("Не знаю як можно викликати цей елс але окей. ЦЕ ПОМИЛКА")
--- sql_scripts.py
+++ sql_scripts.py
@@ -1,10 +1,17 @@
+#підключення бібліотеки sqlite3 для роботи з базами даних
 import sqlite3
 
+
+#створення классу ShopDB для взаємодії з базою даних
 class ShopDB:
+    #параметри обьекту
     def __init__(self, dbname):
+        #встановлення звязку з базою даних
         self.connection = sqlite3.connect(dbname)
+        #створення обьекту звязку з базою даних
         self.cursor = self.connection.cursor()
 
+    #я не буду писати пояснення для кожної функції тут тому що частіше вони виконують дуже вюзьконаправлену задачу
     def is_email_exist(self, email):
         self.cursor.execute('''SELECT email FROM clients WHERE email=?''', [email])
         is_email = self.cursor.fetchone()
@@ -42,8 +49,25 @@ class ShopDB:
         item_db = self.cursor.fetchone()
         return item_db
     
-    def insert_id(self, item_id):
-        self.cursor.execute('''INSERT INTO cart("item_id")
-                            VALUES(?)''',
-                            [item_id])
+    def add_item_to_cart(self, order_id, item_id, quantity):
+        self.cursor.execute('''INSERT INTO cart(order_id, item_id, quantity)
+                            VALUES(?,?,?)''',
+                            [order_id, item_id, quantity])
         self.connection.commit()
+    
+    def create_order(self, client_id):
+        self.cursor.execute('''INSERT INTO orders("client_id")
+                            VALUES(?)''',
+                            [client_id])
+        self.connection.commit()
+        return self.cursor.lastrowid
+    
+    def search_id_in_emails(self, email):
+        self.cursor.execute("""SELECT id FROM clients WHERE email = ?""", (email,))
+        data = self.cursor.fetchone()
+        return data
+    
+    def game_price(self, cart_item):
+        self.cursor.execute("""SELECT price FROM items WHERE title = ?""", (cart_item,))
+        data = self.cursor.fetchone()
+        return data
